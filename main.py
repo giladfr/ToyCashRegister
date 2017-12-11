36e931acb26465969bc50c1652a0ab288b8af253
Gilad Fride <giladfr@gmail.com>
2017-12-11 22:49:10 +0200

    WIP

--- main.py
+++ main.py
@@ -10,17 +10,39 @@ class MainWindow(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.initSignals()
+        self.key_buf = []
+        self.ui.codeBox.setFocus()
+        self.total_price = 0
 
     def initSignals(self):
         self.ui.quitButton.clicked.connect(self.close)
+        self.ui.codeBox.returnPressed.connect(self.search_add_prod)
 
 
+    def search_add_prod(self):
+        search_code = self.ui.codeBox.text()
+        self.ui.codeBox.setText("")
+        print("Searching and adding code %s"%search_code)
+        # TODO
+        price = 5
+        human_name = "Milk"
+        ##
+        self.add_to_list(human_name,price)
+        self.update_total()
 
 
+    def add_to_list(self,product_name,price):
+        self.ui.listProducts.addItem(product_name)
+        self.ui.listProducts.scrollToBottom()
+        self.total_price += price
+
+
+    def update_total(self):
+        self.ui.totalBox.setText("%d"%self.total_price)
 def main():
     app = QtGui.QApplication(sys.argv)
     ex = MainWindow()
-    ex.showFullScreen()
+    ex.show()
     sys.exit(app.exec_())
 
 
